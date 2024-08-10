24cfac0507c4b1f3714363dd204035fd4730ce85
unknown <zelfenimohamediyed@gmail.com>
2024-08-10 13:11:59 +0200

    aa

---
+++ games.js
@@ -0,0 +1,28 @@
+
+
+let images = ['miles.webp', 'michael1.png', '47h.png']
+let indice = 0
+let element = document.getElementById('img4')
+element.addEventListener('dblclick', () =>{
+
+})
+element.addEventListener('click', () => {
+    element.style.opacity = 0;
+    setTimeout(
+        () => {
+            indice = (indice + 1) % images.length;
+            element.src = images[indice];
+            element.style.opacity = 1;
+        }, 500
+    )
+    console.log(indice);
+})
+
+let indi = 0
+let back = ['front5.jpg', 'front1.jpg']
+let btn = document.getElementById('theme')
+let theme = document.querySelector('header')
+btn.addEventListener('click', () => {
+    indi = (indi + 1) % back.length;
+    theme.style.backgroundImage = `url(${back[indi]})`
+})
--- mywebsite.js
+++ mywebsite.js
@@ -1,3 +1,23 @@
-function validateForm(e) {
-return alert(`aaa`)
-}
+function validateForm(event) {
+    event.preventDefault();  
+
+    const name = document.forms["Login"]["name"].value;
+    const email = document.forms["Login"]["email"].value;
+    const password = document.forms["Login"]["password"].value;
+
+    if (name === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    if (email === "") {
+        alert("Email must be filled out");
+        return false;
+    }
+
+    if (password === "") {
+        alert("Password must be filled out");
+        return false;
+    } 
+    window.location.href = "./games.html";
+    return true;}
